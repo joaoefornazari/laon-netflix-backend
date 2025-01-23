0af845c3944f783d30375f0a328267732a559583
joaoefornazari <joaoefornazari@gmail.com>
2025-01-23 16:23:26 -0300

    feat: create-award-table migration

---
+++ database/migrations/2025_01_23_192145_create-award-table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAwardTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('award', function (Blueprint $table) {
+					$table->id();
+					$table->tinyText('name');
+				});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('award');
+    }
+}
