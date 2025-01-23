af4ec4f68a0aa34d7faaa76d66cd4ceb890b2ea6
joaoefornazari <joaoefornazari@gmail.com>
2025-01-23 16:21:30 -0300

    fix: tab spacing

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-			$table->uuid('id');
+						$table->uuid('id');
             $table->string('full_name');
             $table->string('email')->unique();
             $table->string('password');
