09d48974406dea5aad6f044809b5d5112311ca31
joaoefornazari <joaoefornazari@gmail.com>
2025-01-23 17:14:08 -0300

    feat(db): create-media-genre-table migration

---
+++ database/migrations/2025_01_23_193532_create-media-genre-table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediaGenreTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+			Schema::create('media_genre', function (Blueprint $table) {
+				$table->foreign('media_id')->references('id')->on('media');
+				$table->foreign('genre_id')->references('id')->on('genre');
+			});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+			Schema::dropIfExists('media_genre');
+	}
+}
