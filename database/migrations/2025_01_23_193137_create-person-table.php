75340b3de43a083f4623495b502e10ed8615d742
joaoefornazari <joaoefornazari@gmail.com>
2025-01-23 16:34:29 -0300

    feat(db): create-person-table migration

---
+++ database/migrations/2025_01_23_193137_create-person-table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('person', function (Blueprint $table) {
+			$table->id();
+			$table->tinyText('name');
+			$table->string('role');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('person');
+	}
+}
