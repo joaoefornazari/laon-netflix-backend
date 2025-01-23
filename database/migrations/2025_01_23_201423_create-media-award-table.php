a9d1dfbbb73ad959fc0df818b6229db7e9fe843a
joaoefornazari <joaoefornazari@gmail.com>
2025-01-23 17:17:32 -0300

    feat(db): create-media-award-table migration

---
+++ database/migrations/2025_01_23_201423_create-media-award-table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediaAwardTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('media-award', function (Blueprint $table) {
+			$table->foreign('media_id')->references('id')->on('media');
+			$table->foreign('award_id')->references('id')->on('award');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('media-award');
+	}
+}
