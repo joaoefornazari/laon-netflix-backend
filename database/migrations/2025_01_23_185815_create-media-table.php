3420e323225e9b979f8de68dbf8a2c7b82ae13a3
joaoefornazari <joaoefornazari@gmail.com>
2025-01-23 16:15:35 -0300

    feat(db): create-media-table migration

---
+++ database/migrations/2025_01_23_185815_create-media-table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media', function (Blueprint $table) {
+			$table->id();
+			$table->lineString('name');
+			$table->smallInteger('type'); // 1 - filme | 2 - série
+			$table->string('original_title');
+			$table->string('year', 4);
+			$table->number('duration');
+			$table->text('synopsis');
+			$table->string('trailer_link');
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
