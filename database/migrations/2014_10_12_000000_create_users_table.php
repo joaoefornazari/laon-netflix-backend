da3f824bf4bd4f3df27158c193d43c7d0e2207cf
joaoefornazari <joaoefornazari@gmail.com>
2025-01-23 16:31:04 -0300

    fix(db): changing full_name column type to tinyText
    to ensure no overflow problems

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
 						$table->uuid('id');
-            $table->string('full_name');
+            $table->tinyText('full_name');
             $table->string('email')->unique();
             $table->string('password');
             $table->timestamps();
