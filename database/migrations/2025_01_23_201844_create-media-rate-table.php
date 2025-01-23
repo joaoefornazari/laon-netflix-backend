57a5a3663bd188af469643d85543b8896650e004
joaoefornazari <joaoefornazari@gmail.com>
2025-01-23 17:21:42 -0300

    feat(db): create-media-rate-table migration

---
+++ database/migrations/2025_01_23_201844_create-media-rate-table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediaRateTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('media-rate', function (Blueprint $table) {
+			$table->foreign('media_id')->references('id')->on('media');
+			$table->foreign('critic_id')->references('id')->on('critic');
+			$table->number('rate');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('media-rate');
+	}
+}
