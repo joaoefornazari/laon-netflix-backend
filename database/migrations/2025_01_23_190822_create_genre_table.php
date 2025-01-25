275b2d88aa2f739796124e5bb9f8b2ba0040aca6
joaoefornazari <joaoefornazari@gmail.com>
2025-01-25 16:40:53 -0300

    chore: standardizing underscores on file names, missing timestamps added

--- database/migrations/2025_01_23_190822_create_genre_table.php
+++ database/migrations/2025_01_23_190822_create_genre_table.php
@@ -16,6 +16,7 @@ class CreateGenreTable extends Migration
 			Schema::create('genre', function (Blueprint $table) {
 				$table->id();
 				$table->string('name');
+				$table->timestamps();
 			});
 		}
 
