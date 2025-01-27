c5004a610e3fd40a4fc38037ab5a3755337a7663
joaoefornazari <joaoefornazari@gmail.com>
2025-01-27 20:33:37 -0300

    feat: defining actual primary key type

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,8 @@ class User extends Authenticatable
         'password',
     ];
 
+		protected $keyType = 'string';
+
     /**
      * The attributes that should be hidden for serialization.
      *
