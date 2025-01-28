5deb4169f7dfb71589ec1903834020509f4424e3
joaoefornazari <joaoefornazari@gmail.com>
2025-01-27 22:17:24 -0300

    feat: updating login steps in controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,33 +1,39 @@
 <?php
  
 namespace App\Http\Controllers;
- 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
- 
+use App\Http\Traits\APIResponse;
+use Exception;
+
 class LoginController extends Controller
 {
-    /**
-     * Handle an authentication attempt.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function authenticate(Request $request)
-    {
-        $credentials = $request->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required'],
-        ]);
- 
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
- 
-            return redirect()->intended();
-        }
- 
-        return back()->withErrors([
-            'email' => 'The provided credentials do not match our records.',
-        ]);
-    }
+	use APIResponse;
+
+	/**
+	 * Handle an authentication attempt.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function authenticate(Request $request)
+	{
+		$credentials = $request->validate([
+			'email' => ['required', 'email'],
+			'password' => ['required'],
+		]);
+
+		try {
+			if (!Auth::attempt($credentials)) {
+				throw new Exception('User not found. Check credentials again.', 404);
+			} else {	
+				$user = Auth::user();
+				return $this->success(['uuid' => $user->id], "User logged in succesfully.", 200);
+			}
+		} catch (Exception $e) {
+			return $this->error($e->getTrace(), $e->getMessage(), $e->getCode());	
+		}
+				
+	}
 }
