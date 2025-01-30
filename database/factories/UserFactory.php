4647bb8a07b823910c582261fecc5941317c2004
joaoefornazari <joaoefornazari@gmail.com>
2025-01-30 13:45:29 -0300

    feat: User factory implementation defined

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,9 +15,9 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'full_name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('test123'), // password
         ];
     }
 }
