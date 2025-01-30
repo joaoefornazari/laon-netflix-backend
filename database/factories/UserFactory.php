a5915cb6228662825db0422116a9a6e13832f738
joaoefornazari <joaoefornazari@gmail.com>
2025-01-30 20:26:59 -0300

    feat: adding uuid to user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Ramsey\Uuid\Rfc4122\UuidV4;
 
 class UserFactory extends Factory
 {
@@ -15,6 +16,7 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
+						'id' => UuidV4::uuid4()->toString(),
             'full_name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'password' => bcrypt('test123'), // password
