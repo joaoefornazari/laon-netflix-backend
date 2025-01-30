b42ca5b2405b81b24bfdbb6e31df5ece834610b5
joaoefornazari <joaoefornazari@gmail.com>
2025-01-29 23:46:56 -0300

    feat: MediaGenre ALD API flow
    ALD - add, list, delete

---
+++ app/Http/Services/MediaGenreService.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Services;
+
+use App\Models\Genre;
+use App\Models\MediaGenre;
+use Illuminate\Support\ServiceProvider;
+
+class MediaGenreService extends ServiceProvider
+{
+	protected $mediaGenre;
+	
+	public function __construct(MediaGenre $mediaGenre)
+	{
+		$this->mediaGenre = $mediaGenre;
+	}
+
+	/**
+	 * Get media genres by media id.
+	 * @param int $mediaId The Media id.
+	 * @return array
+	 */
+	public function getMediaGenres(int $mediaId)
+	{
+		$query = $this->mediaGenre->query();
+		$mediaGenres = $query
+			->where('media_id', $mediaId)
+			->get(['genre_id'])
+			->toArray();
+
+		$genres = (new Genre())->query()
+			->whereIn('id', array_column($mediaGenres, 'genre_id'))
+			->get(['id', 'name'])
+			->toArray();
+
+		for ($i = 0; $i < count($genres); $i++) {
+			$mediaGenres[$i]['name'] = $genres[$i]['name'];
+		}
+
+		return $mediaGenres;
+	}
+
+	/**
+	 * Create a new media genre and return as an array.
+	 * @param int $mediaId The Media id.
+	 * @param int $genreId The Genre id.
+	 * @return array
+	 */
+	public function createMediaGenre(int $mediaId, int $genreId)
+	{
+		$mediaGenre = new MediaGenre();
+		$mediaGenre->media_id = $mediaId;
+		$mediaGenre->genre_id = $genreId;
+		$mediaGenre->save();
+
+		return $mediaGenre->toArray();
+	}
+
+	/**
+	 * Delete a specific genre from a specific media.
+	 * @param int $mediaId The Media id.
+	 * @param int $genreId The Genre id.
+	 * @return array
+	 */
+	public function deleteMediaGenre(int $mediaId, int $genreId)
+	{
+		$query = $this->mediaGenre->query();
+		$query
+			->where('media_id', $mediaId)
+			->where('genre_id', $genreId)
+			->delete();
+
+		return ['media_id' => $mediaId, 'genre_id' => $genreId];
+	}
+}
